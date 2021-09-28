6d9dd5000da3084b0dfc99041c7acdb1bd9d1530
shotaroonishi <o_onishi@kani-design.com>
2021-09-28 14:50:58 +0900

    cursor変更

--- js/main.js
+++ js/main.js
@@ -19,3 +19,22 @@ activeArea.addEventListener("mousemove", (e) => {
     target[i].style.transform = `translate(${x}px, 0px)`;
   }
 });
+
+
+// カーソル用のdivタグを取得してcursorに格納
+var cursor = document.getElementById('cursor'); 
+
+// カーソル用のdivタグをマウスに追従させる
+document.addEventListener('mousemove', function (e) {
+    cursor.style.transform = 'translate(' + e.clientX + 'px, ' + e.clientY + 'px)';
+});
+
+// リンクにホバーした時にクラス追加、離れたらクラス削除
+var link = document.querySelectorAll('a');
+for (var i = 0; i < link.length; i++) {
+    link[i].addEventListener('mouseover', function (e) {
+        cursor.classList.add('cursor--hover');
+    });
+    link[i].addEventListener('mouseout', function (e) {
+        cursor.classList.remove('cursor--hover');   
+    });}
